3ca130bfc1237baf7470c9a88dd7075a8b5db67b
E <emilakbarov857@gmail.com>
2025-07-22 23:57:11 +0400

    first

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(BOT_TOKEN)
 
+
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id,f"{message.from_user.username}")
+    bot.send_message(message.chat.id,f"Hello {message.from_user.username}")
 
 @bot.message_handler(commands=['help'])
 def start(message):

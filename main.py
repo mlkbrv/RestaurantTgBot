dfef4b9ba013879ad7bd24f8bab0d397b742a698
E <emilakbarov857@gmail.com>
2025-07-27 22:47:02 +0400

    second

--- main.py
+++ main.py
@@ -15,9 +15,18 @@ def start(message):
 def start(message):
     bot.send_message(message.chat.id,"<b>Help</b> <u>Information</u>",parse_mode='html')
 
-@bot.message_handler(content_types=['github'])
+@bot.message_handler(commands=['github'])
 def github(message):
     bot.reply_to(message,f"https://github.com/mlkbrv")
+
+@bot.message_handler(content_types=['photo'])
+def photo(message):
+    markup = telebot.types.InlineKeyboardMarkup()
+    markup.add(telebot.types.InlineKeyboardButton("Go to site",url="https://github.com/mlkbrv"))
+    btn1 = telebot.types.InlineKeyboardButton("Delete Photo",callback_data="delete")
+    btn2 = telebot.types.InlineKeyboardButton("ChangeText",callback_data="edit")
+    markup.row(btn1,btn2)
+    bot.reply_to(message, "Какое красивое фото", reply_markup=markup)
 
 @bot.message_handler()
 def temp(message):
@@ -25,5 +34,7 @@ def temp(message):
         bot.send_message(message.chat.id, f"Hello {message.from_user.username}")
     elif message.text.lower() == "id":
         bot.reply_to(message, f"ID {message.from_user.id}")
+
+bot.remove_webhook()
 
 bot.polling(none_stop=True)

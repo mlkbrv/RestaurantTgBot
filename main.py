baf26e8651d289dcb0099e0abee6d282bbe8edb8
E <emilakbarov857@gmail.com>
2025-07-27 23:22:34 +0400

    more buttons

--- main.py
+++ main.py
@@ -9,7 +9,21 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id,f"Hello {message.from_user.username}")
+    markup = telebot.types.ReplyKeyboardMarkup()
+    markup.add(telebot.types.KeyboardButton("Go to site"))
+    btn1 = telebot.types.KeyboardButton("Delete")
+    btn2 = telebot.types.KeyboardButton("ChangeText")
+    markup.row(btn1, btn2)
+    file = open('./1.pdf','rb')
+    bot.send_document(message.chat.id,file,reply_markup=markup)
+    # bot.send_message(message.chat.id, "Welcome to the bot", reply_markup=markup)
+    bot.register_next_step_handler(message,on_click)
+
+def on_click(message):
+    if message.text == "Go to site":
+        bot.send_message(message.chat.id, "https://github.com/mlkbrv")
+    elif message.text == "Delete Photo":
+        bot.delete_message(message.chat.id, message.id-1)
 
 @bot.message_handler(commands=['help'])
 def start(message):
@@ -35,6 +49,12 @@ def temp(message):
     elif message.text.lower() == "id":
         bot.reply_to(message, f"ID {message.from_user.id}")
 
+@bot.callback_query_handler(func=lambda callback: True)
+def callback_message(callback):
+    if callback.data == "delete":
+        bot.delete_message(callback.message.chat.id, callback.message.message_id-1)
+    elif callback.data == "edit":
+        bot.edit_message_text("Edited text", callback.message.chat.id,callback.message.message_id)
 bot.remove_webhook()
 
 bot.polling(none_stop=True)
